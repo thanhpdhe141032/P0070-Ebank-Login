9cf732c97f35b8df88cdfd163d0f02228a511d47
thanhdev <thanhpdhe141032@fpt.edu.vn>
2023-06-09 14:12:07 +0700

    update

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+J
+src/language/vi.properties,9\7\9720907d4321a33ce8c1a9fa12a6e0422dd2bff8
--- src/service/Services.java
+++ src/service/Services.java
@@ -5,12 +5,8 @@ import java.util.ResourceBundle;
 
 
 public class Services {
-    private static final char[] chars = {'1', 'A', 'a', 'B', 'b', 'C',
-            'c', '2', 'D', 'd', 'E', 'e', 'F', 'f', '3', 'G', 'g', 'H', 'h',
-            'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', '4', 'M', 'm', 'N', 'n',
-            'O', 'o', '5', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't',
-            '6', '7', 'U', 'u', 'V', 'v', 'U', 'u', 'W', 'w', '8', 'X', 'x',
-            'Y', 'y', 'Z', 'z', '9'};
+    private static final char[] chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
 
     public String generateCaptchaText() {
         final int LENGTH = 6;
